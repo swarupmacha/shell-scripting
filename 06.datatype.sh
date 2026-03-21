4c2197a6c94f07c387b370615f5e0ed9b8412036
Your Name <youremail@example.com>
2026-03-21 13:10:12 +0530

    six file commit

---
+++ 06.datatype.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+NUM1=100
+NUM2=200
+NUM3=dcfvgbhnjk
+SUM=$(($NUM2-+NUM1))
+
+SUB=$(($NUM2+$NUM3))
+echo "sum is : $SUM"
+
+FRUITS=("apple" "Banana" "free")
+
+echo "fruits are : ${FRUITS[0]}"
+echo "fruits are : ${FRUITS[1]}"
+echo "fruits are : ${FRUITS[2]}"
