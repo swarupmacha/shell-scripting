3926f7ddd4626fc003d32a18d5bb48397574d2a5
Your Name <youremail@example.com>
2026-03-21 16:46:48 +0530

    fivecommit

--- 10.logs.sh
+++ 10.logs.sh
@@ -11,10 +11,10 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE"
+        echo "$2 ... FAILURE"  | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo "$2 ... SUCCESS"  | tee -a $LOGS_FILE
     fi
 }
 
