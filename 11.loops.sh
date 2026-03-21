ddc057f1b6a01a8e4434e4de379d99a4120c155d
Your Name <youremail@example.com>
2026-03-21 16:56:55 +0530

    thirdth commit

--- 11.loops.sh
+++ 11.loops.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for i in {1..100}
+for i in {1..5}
 do
     echo $i
+done
+
+for package in $@
+do 
+   if [ $? -ne 0 ]; then 
+        echo "Installing $package ... FAILURE"
+        exit 1
+   else
+        echo "Installing $package ... SUCCESS"
+    fi
 done
