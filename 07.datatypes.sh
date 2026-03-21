40837010cbd7589e10831975a517d6a395b7dea4
Your Name <youremail@example.com>
2026-03-21 13:21:19 +0530

    seventh file

---
+++ 07.datatypes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM=25
+
+if [$NUM -gt 20 ]; then 
+   echo "given number is gretart than 20"
+
+elif [$NUM -eq 20]; then
+   echo "number is eqyal to 20"
+else 
+   echo ":givem os less than 20"
+fi
